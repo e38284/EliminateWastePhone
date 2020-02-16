07f781a53c269df34a4596ba9e5e637dcc7f4d85
yutaro.hagiwara <s21161062@gmail.com>
2020-02-16 23:51:35 +0900

    Initial Commit

---
+++ EliminateWastePhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EliminateWastePhone
+//
+//  Created by YutaroHagiwara on 2020/02/16.
+//  Copyright © 2020 YutaroHagiwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
